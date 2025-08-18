eb501d1e384521c0c6886a8c791bd9d3951bb2e5
JosueHenriquez <52579357+JosueHenriquez@users.noreply.github.com>
2025-08-18 09:43:59 -0600

    Delete src/main/java/IntegracionBackFront/backfront/Controller/Cloudinary directory

--- src/main/java/IntegracionBackFront/backfront/Controller/Cloudinary/ImageController.java
+++
@@ -1,9 +0,0 @@
-package IntegracionBackFront.backfront.Controller.Cloudinary;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/api/image")
-public class ImageController {
-}
